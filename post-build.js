754dd3472db628da04185b1c2df4ec3a5ec7d8cd
gitlab-az1 <gitlab.az1@gmail.com>
2025-01-15 21:23:55 -0300

    fix: removed unecessary entries on build dir

--- post-build.js
+++ post-build.js
@@ -8,6 +8,25 @@ const BUILD_DIR = path.resolve('./dist/');
 
 
 (async function() {
+  async function rimraf(pathname) {
+    const stat = await fs.promises.stat(pathname);
+    if(!stat.isDirectory()) return fs.promises.unlink(pathname);
+
+    const contents = await fs.promises.readdir(pathname);
+
+    for(const entry of contents) {
+      const currentPath = path.join(pathname, entry);
+      const currentStat = await fs.promises.stat(currentPath);
+
+      if(currentStat.isDirectory()) {
+        await rimraf(currentPath);
+        continue;
+      }
+
+      await fs.promises.unlink(currentPath);
+    }
+  }
+
   async function rrmjs(pathname = BUILD_DIR) {
     try {
       const stat = await fs.promises.stat(pathname);
@@ -35,4 +54,6 @@ const BUILD_DIR = path.resolve('./dist/');
   }
 
   await rrmjs();
+  await rimraf(path.join(BUILD_DIR, '.vscode'));
+  await rimraf(path.join(BUILD_DIR, '.editorconfig'));
 })();
