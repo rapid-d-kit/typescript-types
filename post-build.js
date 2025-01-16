50916ac3fe161208b20c7c2e4a8211f3fffc6caa
gitlab-az1 <gitlab.az1@gmail.com>
2025-01-15 21:27:14 -0300

    fix (#2)

--- post-build.js
+++ post-build.js
@@ -9,32 +9,34 @@ const BUILD_DIR = path.resolve('./dist/');
 
 (async function() {
   async function rimraf(pathname) {
-    const stat = await fs.promises.stat(pathname);
+    try {
+      const stat = await fs.promises.stat(pathname);
 
-    if(!stat.isDirectory()) {
-      try {
+      if(!stat.isDirectory()) {
         await fs.promises.unlink(pathname);
-      } catch (err) {
-        if(err.code === 'ENOENT') return;
-        throw err;
-      }
-    }
-
-    const contents = await fs.promises.readdir(pathname);
-
-    for(const entry of contents) {
-      const currentPath = path.join(pathname, entry);
-      const currentStat = await fs.promises.stat(currentPath);
-
-      if(currentStat.isDirectory()) {
-        await rimraf(currentPath);
-        continue;
+        return;
       }
 
-      try {
-        await fs.promises.unlink(currentPath);
-      } catch (err) {
-        if(err.code === 'ENOENT') continue;
+      const contents = await fs.promises.readdir(pathname);
+
+      for(const entry of contents) {
+        const currentPath = path.join(pathname, entry);
+        const currentStat = await fs.promises.stat(currentPath);
+
+        if(currentStat.isDirectory()) {
+          await rimraf(currentPath);
+          continue;
+        }
+
+        try {
+          await fs.promises.unlink(currentPath);
+        } catch (err) {
+          if(err.code === 'ENOENT') continue;
+          throw err;
+        }
+      }
+    } catch (err) {
+      if(err.code !== 'ENOENT') {
         throw err;
       }
     }
